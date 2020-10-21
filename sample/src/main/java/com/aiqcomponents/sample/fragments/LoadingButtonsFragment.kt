a590cc962196da5cafef15815e94fb4414614e23
Lucas Villa Verde <lucasgabrielvverde@gmail.com>
2020-10-20 23:23:49 -0300

    Refactored Loading Buttons sample

---
+++ sample/src/main/java/com/aiqcomponents/sample/fragments/LoadingButtonsFragment.kt
@@ -0,0 +1,95 @@
+package com.aiqcomponents.sample.fragments
+
+import android.graphics.Color
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.aiqcomponents.sample.R
+import com.aiqfome.aiqcomponents.progressbutton.DrawableButton
+import com.aiqfome.aiqcomponents.progressbutton.ProgressButtonUtils.Companion.hideProgress
+import com.aiqfome.aiqcomponents.progressbutton.ProgressParams
+import com.aiqfome.aiqcomponents.progressbutton.showProgress
+import kotlinx.android.synthetic.main.fragment_loading_buttons.*
+import kotlinx.android.synthetic.main.fragment_loading_buttons.view.*
+
+class LoadingButtonsFragment : Fragment(R.layout.fragment_loading_buttons) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpClickListeners()
+    }
+
+    private fun setUpClickListeners() {
+        layoutLoadingButtonsRoot.btnProgressLeft?.setOnClickListener {
+            showProgressLeft(layoutLoadingButtonsRoot.btnProgressLeft)
+        }
+        layoutLoadingButtonsRoot.btnProgressCenter?.setOnClickListener {
+            showProgressCenter(layoutLoadingButtonsRoot.btnProgressCenter)
+        }
+        layoutLoadingButtonsRoot.btnProgressRight?.setOnClickListener {
+            showProgressRight(layoutLoadingButtonsRoot.btnProgressRight)
+        }
+        layoutLoadingButtonsRoot.btnProgressCustom?.setOnClickListener {
+            showProgressCustom(layoutLoadingButtonsRoot.btnProgressCustom)
+        }
+    }
+
+    private fun showProgressLeft(button: Button) {
+        button.showProgress { progressParams: ProgressParams ->
+            progressParams.buttonTextRes = R.string.loading
+            progressParams.progressColor = Color.WHITE
+            progressParams.gravity = DrawableButton.GRAVITY_TEXT_START
+        }
+        button.isEnabled = false
+        Handler(Looper.getMainLooper()).postDelayed({
+            button.isEnabled = true
+            hideProgress(button, R.string.progress_left_text)
+        }, 3000)
+    }
+
+    private fun showProgressCenter(button: Button) {
+        button.showProgress { progressParams: ProgressParams ->
+            progressParams.progressColor = Color.WHITE
+            progressParams.gravity = DrawableButton.GRAVITY_CENTER
+        }
+        button.isEnabled = false
+        Handler(Looper.getMainLooper()).postDelayed({
+            button.isEnabled = true
+            hideProgress(button, R.string.progress_center_text)
+        }, 3000)
+    }
+
+    private fun showProgressRight(button: Button) {
+        button.showProgress { progressParams: ProgressParams ->
+            progressParams.buttonTextRes = R.string.loading
+            progressParams.progressColor = Color.WHITE
+        }
+        button.isEnabled = false
+        Handler(Looper.getMainLooper()).postDelayed({
+            button.isEnabled = true
+            hideProgress(button, R.string.progress_right_text)
+        }, 3000)
+    }
+
+    private fun showProgressCustom(button: Button) {
+        button.showProgress { progressParams: ProgressParams ->
+            progressParams.buttonTextRes = R.string.loading
+            progressParams.progressColors = intArrayOf(Color.WHITE, Color.MAGENTA, Color.GREEN)
+            progressParams.gravity = DrawableButton.GRAVITY_TEXT_END
+            progressParams.progressRadiusRes = R.dimen.progressRadius
+            progressParams.progressStrokeRes = R.dimen.progressStroke
+            progressParams.textMarginRes = R.dimen.textMarginStyled
+        }
+        button.isEnabled = false
+        Handler(Looper.getMainLooper()).postDelayed({
+            button.isEnabled = true
+            hideProgress(button, R.string.progress_custom_text)
+        }, 3000)
+    }
+
+
+}
