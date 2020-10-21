179fcdd1eed5fe16bd9823fe587ca22809270d28
Lucas Villa Verde <lucasgabrielvverde@gmail.com>
2020-10-21 00:01:56 -0300

    Added navigation component

---
+++ sample/src/main/java/com/aiqcomponents/sample/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.aiqcomponents.sample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
