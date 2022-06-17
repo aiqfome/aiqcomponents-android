bfa528f20e69176642e35bd12f6cb9338e3a8f17
Matheus H. de Souza <mh.matheussouza@gmail.com>
2022-06-17 18:00:58 -0300

    refactor: rm loading buttons

--- sample/src/main/java/com/aiqcomponents/sample/models/ComponentItem.kt
+++ sample/src/main/java/com/aiqcomponents/sample/models/ComponentItem.kt
@@ -2,4 +2,8 @@ package com.aiqcomponents.sample.models
 
 import android.view.View
 
-class ComponentItem (val name: String, val clickListener: View.OnClickListener, val description: String)
+class ComponentItem(
+    val name: String,
+    val clickListener: View.OnClickListener,
+    val description: String
+)
